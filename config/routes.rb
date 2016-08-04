3f6f49b7558e859aadb04e5707c328c0ea60184a
Kate Walters <kawalters01@gmail.com>
2016-08-04 16:02:16 -0500

    Added dashboard controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :photos
   resources :notes
   resources :categories
+  root'dashboard#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
