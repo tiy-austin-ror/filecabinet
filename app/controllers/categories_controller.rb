02e8a528e6695b2002374971d805f55533c3658d
Ahkeem Lang <langahkeem@gmail.com>
2016-08-04 16:31:18 -0500

    Finished required CRUD actions for category controller. Created category view folder

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,48 @@
 class CategoriesController < ApplicationController
+  def new
+    render locals: {
+      category: Category.new
+    }
+  end
+
+  def create
+    category = Category.new
+    category.name = params[:category][:name]
+
+    if category.save
+      redirect_to actions: "index"
+    else
+      flash[:alert] = "Category could not be saved."
+      render template: 'category/new.html.erb', locals: {
+        category: category
+      }
+    end
+  end
+
+  def edit
+    render locals: {
+      category: Category.find(params[:id])
+    }
+  end
+
+  def update
+    category = Category.find(params[:id])
+    category.name = params[:category][:name] if params[:category][:name].present?
+    if category.saved
+      redirect_to user_path(path)
+    else
+      render templatee: '/category/new.html.erb', locals: {
+        category: category
+      }
+    end
+  end
+
+  def destroy
+    if Category.exists?(params[:id])
+      Category.destroy(params[:id])
+      render message: "Category deleted."
+    else
+      render message: "Category not found."
+    end
+  end
 end
