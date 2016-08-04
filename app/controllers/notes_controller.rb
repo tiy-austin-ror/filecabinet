1e535957f121b6937d38352713b36822cfd46a76
Robert Luckfield <rluck0419@gmail.com>
2016-08-04 16:54:10 -0500

    create note story

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,60 @@
 class NotesController < ApplicationController
+  def index
+    notes = Note.all
+    render locals: { notes: notes }
+  end
+
+  def show
+    if Note.exists?(params[:id])
+      note = Note.find(params[:id])
+      render locals: { note: note }
+    else
+      render html: 'Note not found', status: 404
+    end
+  end
+
+  def new
+    render locals: { note: Note.new }
+  end
+
+  def create
+    note = Note.new(note_params)
+    if note.save
+      redirect_to note
+    else
+      render :new, locals: { note: note }
+    end
+  end
+
+  def edit
+    render locals: { note: Note.find(params[:id]) }
+  end
+
+  def update
+    if Note.exists?(params[:id])
+      note = Note.find(params[:id])
+      if note.update(note_params)
+        redirect_to note
+      else
+        render :edit
+      end
+    else
+      render html: 'Note not found', status: 404
+    end
+  end
+
+  def destroy
+    if Note.exists?(params[:id])
+      Note.destroy(params[:id])
+      flash[:notice] = "Note deleted"
+      redirect_to notes
+    else
+      flash[:alert] = "There was an error - please try again"
+    end
+  end
+
+  private
+  def note_params
+    params.require(:note).permit(:user_id, :category_id, :name, :body, :file_type)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+NOTE_NAMES = ["Board_Meeting_2016-08-04", "Scrum_2016-08-04", "Sprint_Review_2016-08-04", "Employee_Evaluations_2016-08-04"]
+NOTE_BODIES = ["We need to shut down the company - immediately", "Everyone is blocked by the pile-up of back-end stories", "nothing worked well - back to the drawing board for the next sprint", "Our employees suck - let's clean the house and start fresh"]
+NOTE_TYPES = ["txt", "xml", "csv", "pdf"]
+
+NOTE_NAMES.each_with_index do |name, i|
+  Note.create!(name: name, body: NOTE_BODIES[i], file_type: NOTE_TYPES[i], category_id: 1, user_id: 1)
+end
