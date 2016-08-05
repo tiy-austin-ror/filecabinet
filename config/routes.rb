c8531c18702cf310e67764c4523ac58cb9fc1f06
Matt Rice <mattrice12@outlook.com>
2016-08-05 12:57:59 -0500

    changed root path

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
 
 
 
-  get '/' => 'users#index', as: 'root'
+  root 'dashboard#index', as: 'root'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
