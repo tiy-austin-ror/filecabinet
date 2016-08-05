a78c461065f700e68cdfbb6d3a97d5ea3cb0baed
Robert Luckfield <rluck0419@gmail.com>
2016-08-05 16:16:23 -0500

    fixed inapp. photo and fixed forms

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -18,7 +18,7 @@ class PhotosController < ApplicationController
   end
 
   def create
-    photo = Photo.new(photo_params)
+    photo = current_user.photos.build(photo_params)
     if photo.save
       redirect_to photo
     else
