445a3141cffa5fdf238300be80ad3957f13bc2c7
Matt Rice <mattrice12@outlook.com>
2016-08-04 20:49:28 -0500

    added authentication -- routes, controllers, views, models. Added Admin in seeds

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,16 @@ Rails.application.routes.draw do
   resources :photos
   resources :notes
   resources :categories
+
+  resources :passwords, controller: 'passwords', only: [:create, :new]
+  resource  :session,   controller: 'sessions',  only: [:create]
+  resources :users,     controller: 'users',     only: Clearance.configuration.user_actions do
+    resource  :password,  controller: 'passwords', only: [:create, :edit, :update]
+  end
+
+  get    '/sign_in'     => 'sessions#new',     as: 'sign_in'
+  delete '/sign_out'    => 'sessions#destroy', as: 'sign_out'
+  get    '/sign_up'     => 'users#new',        as: 'sign_up'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
