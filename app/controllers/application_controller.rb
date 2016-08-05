ee5167a88045b093d128fc3f7b65c0a0055cd0dd
Kate Walters <kawalters01@gmail.com>
2016-08-05 15:54:06 -0500

    Added authorization to notes and photos, only that user or admin can view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   include Clearance::Controller
   protect_from_forgery with: :exception
+
+  def restrict_non_admin(redirect_path)
+    unless admin_user?
+      flash[:alert] = "You do not have permission to view this page."
+      redirect_to redirect_path
+    end
+  end
 end
