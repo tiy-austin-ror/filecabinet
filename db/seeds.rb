c78053234ad7c8db95dc06d8a6589c9fc8aa63a7
Matt Rice <mattrice12@outlook.com>
2016-08-07 03:10:43 -0500

    made seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,19 @@
 User.create!(name: "admin", email: "admin@example.com", password: "password", admin: true)
 
-10.times do
+100.times do
   Tag.create(name: Faker::Commerce.color)
 end
 
 
 10.times do
   Category.create!(name: Faker::Company.buzzword)
+  10.times do
+    Category.create!(name: Faker::Company.buzzword, parent_category_id: rand(1..Category.count))
+  end
 end
+
+
+
 NOTE_NAMES = ["Board_Meeting_2016-08-04", "Scrum_2016-08-04", "Sprint_Review_2016-08-04", "Employee_Evaluations_2016-08-04"]
 NOTE_BODIES = ["We need to shut down the company - immediately", "Everyone is blocked by the pile-up of back-end stories", "nothing worked well - back to the drawing board for the next sprint", "Our employees suck - let's clean the house and start fresh"]
 NOTE_TYPES = ["txt", "md"]
@@ -15,8 +21,9 @@ NOTE_TYPES = ["txt", "md"]
 PHOTO_NAMES = ["White_Board_2016-08-04", "Meeting_Notes_2016-08-04", "Sprint_Review_Board_2016-08-04", "Scrum_Notes_2016-08-04"]
 PHOTO_DESCS = ["White board photo from the meeting", "Photo-notes from the board meeting", "Photo of white board from sprint review", "Photo-notes from daily scrum"]
 
-
-NOTE_NAMES.each_with_index do |name, i|
-  Note.create!(name: name, body: NOTE_BODIES[i], file_type: NOTE_TYPES.sample, category_id: 1, user_id: 1)
-  Photo.create!(name: PHOTO_NAMES[i], desc: PHOTO_DESCS[i], category_id: 1, user_id: 1, upload: "http://imgs.xkcd.com/comics/tags.png")
+50.times do
+  NOTE_NAMES.each_with_index do |name, i|
+    Note.create!(name: name, body: NOTE_BODIES[i], file_type: NOTE_TYPES.sample, category_id: rand(1..Category.count), user_id: 1)
+    Photo.create!(name: PHOTO_NAMES[i], desc: PHOTO_DESCS[i], category_id: rand(1..Category.count), user_id: 1, upload: "http://imgs.xkcd.com/comics/tags.png")
+  end
 end
