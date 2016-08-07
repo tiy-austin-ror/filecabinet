6bb51b24857283df45193d6d00da6b5c82f65843
Kate Walters <kawalters01@gmail.com>
2016-08-06 19:33:52 -0500

    Updated destroy method in permissions controller with user_id

--- app/controllers/permissions_controller.rb
+++ app/controllers/permissions_controller.rb
@@ -1,6 +1,6 @@
 class PermissionsController < ApplicationController
   def destroy
-    if has_permission?
+    if permission.user_id.include? current_user.id
         permission.destroy #delete permission given by someone else
         flash[:notice] = "This user no longer has extra permissions."
         redirect_to root_path
@@ -8,10 +8,5 @@ class PermissionsController < ApplicationController
       flash[:alert] = "You cannot delete this permission."
       redirect_to root_path
     end
-  end
-
-  private
-  def has_permission?(object)
-    object.user_id == current_user.id || current_user.admin?
   end
 end
