6c9dfff2255c0766f499629b3c4b8d013beaa580
Matt Rice <mattrice12@outlook.com>
2016-08-08 01:23:17 -0500

    can now create categories inside other categories.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -62,6 +62,6 @@ class CategoriesController < ApplicationController
 
   private
   def category_params
-    params.require(:category).permit(:name, params[:parent_category_id])
+    params.require(:category).permit(:name, :parent_category_id)
   end
 end
