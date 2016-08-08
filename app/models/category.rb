97262ef4a69b16d8cc1b20d6bcf6aa787cb87f90
Matt Rice <mattrice12@outlook.com>
2016-08-08 14:39:42 -0500

    fixed categories/show to include category path; restructured show category controller method and category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -6,4 +6,22 @@ class Category < ApplicationRecord
   belongs_to :parent, class_name: "Category", foreign_key: :parent_category_id, optional: true
 
   validates :name, presence: true
+
+  def ancestors
+    cats = []
+    current_cat = self
+    while current_cat do
+      cats << current_cat
+      current_cat = current_cat.parent
+    end
+
+    cats.reverse!.pop
+    cats
+  end
+
+  # def ancestors(cats = [])
+  #   cats.push(self)
+  #   return cats.drop(1).reverse if parent.nil?
+  #   parent.ancestors(cats)
+  # end
 end
