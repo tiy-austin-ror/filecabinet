d965f355cb85164dd97ba5668d2a7f7bfc76823d
Ahkeem Lang <langahkeem@gmail.com>
2016-08-09 12:05:31 -0500

    Added photos each do method to the index.html.erb file.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,8 @@ class DashboardController < ApplicationController
   before_action :require_login
   def index
     render locals: {
-      notes: Note.where(user: current_user)
+      notes: Note.where(user: current_user),
+      photos: Photo.where(user: current_user)
     }
   end
 end
