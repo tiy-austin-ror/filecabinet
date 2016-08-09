e2a574799f3d83817f5d88e140e4800bf4cbfed1
Matt Rice <mattrice12@outlook.com>
2016-08-09 17:42:42 -0500

    reduced seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,13 @@ User.create!(name: "user", email: "user@example.com", password: "password", admi
 end
 
 
-10.times do
+5.times do
   Category.create!(name: Faker::Company.buzzword)
-  10.times do
+  2.times do
     Category.create!(name: Faker::Company.buzzword, parent_category_id: rand(1..Category.count))
   end
 end
 
-10.times do
-  Category.create!(name: Faker::Company.buzzword, parent_category_id: rand(1..5))
-end
 
 NOTE_NAMES = ["Board_Meeting_2016-08-04", "Scrum_2016-08-04", "Sprint_Review_2016-08-04", "Employee_Evaluations_2016-08-04"]
 NOTE_BODIES = ["We need to shut down the company - immediately", "Everyone is blocked by the pile-up of back-end stories", "nothing worked well - back to the drawing board for the next sprint", "Our employees suck - let's clean the house and start fresh"]
---
+++ features/category_creation.feature
@@ -0,0 +1,3 @@
+Feature: As an Admin, I would like to be able to create Categories in my location of choice
+
+Background: An Admin Logs In
