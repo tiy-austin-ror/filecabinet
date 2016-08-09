cfeab1901d0834f71c0a10a50e825055deca7cdf
Ahkeem Lang <langahkeem@gmail.com>
2016-08-09 17:15:48 -0500

    Deleted a block of unnecessary code in the dashboard controller.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,14 +4,6 @@ class DashboardController < ApplicationController
     categories = []
     notes = Note.where(user: current_user)
     photos = Photo.where(user: current_user)
-     
-    if params[:query]
-      categories = Category.where("UPPER(name) LIKE UPPER(?)", "%#{params[:query]}%")
-
-      notes = Note.where("UPPER(name) LIKE UPPER(?)", "%#{params[:query]}%")
-
-      photos = Photo.where("UPPER(name) LIKE UPPER(?)", "%#{params[:query]}%")
-    end
     render locals: { categories: categories, notes: notes, photos: photos }
   end
 end
