373377df5c33c8faecfd376621fccddb46d8a965
Kate Walters <kawalters01@gmail.com>
2016-08-09 16:10:43 -0500

    Redirect_to :back after destroy

--- app/controllers/permissions_controller.rb
+++ app/controllers/permissions_controller.rb
@@ -12,13 +12,14 @@ class PermissionsController < ApplicationController
   end
 
   def destroy
-    if object.users_with_access.include?(current_user)
+    permission = Permission.find(params[:id])
+    if has_permission?(permission.permission)
         permission.destroy #delete permission given by someone else
         flash[:notice] = "This user no longer has extra permissions."
-        redirect_to root_path
+        redirect_to :back
     else
       flash[:alert] = "You cannot delete this permission."
-      redirect_to root_path
+      redirect_to :back
     end
   end
 end
@@ -26,4 +27,8 @@ end
 private
 def permission_params
   params.require(:permission).permit(:permission_id, :permission_type, :full_access, :user_id)
+end
+
+def has_permission?(object)
+  object.user_id == current_user.id || current_user.admin?
 end
