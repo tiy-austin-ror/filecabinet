153acf2c475cb80c38ba42d8a9c8a5a311795e49
rluck0419 <ral2576@utexas.edu>
2016-08-10 14:42:24 -0500

    Update photo.rb

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -20,9 +20,4 @@ class Photo < ApplicationRecord
   validates :desc, presence: true
   validates :upload, attachment_presence: true
 
-  def s3_credentials
-    { :bucket => "filecabinet-tiy-ror",
-      :aws_access_key_id => ENV["AWS_ACCESS_KEY_ID"],
-      :aws_secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"] }
-  end
 end
