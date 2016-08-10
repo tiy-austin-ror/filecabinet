5a8c079f1d5db796e3376e11b33fda213aa09f6e
rluck0419 <ral2576@utexas.edu>
2016-08-10 14:42:53 -0500

    Update test.rb

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -18,7 +18,6 @@ Rails.application.configure do
     'Cache-Control' => 'public, max-age=3600'
   }
 
-
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
