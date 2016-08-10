749d5cf9ebb05e8be00ecc788d1b1caae016b67c
Kate Walters <kawalters01@gmail.com>
2016-08-10 14:34:51 -0500

    Admins can now edit teams

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,5 @@
 class Team < ApplicationRecord
-  belongs_to :users
+  belongs_to :users, optional: true
   has_many :team_members, through: :members, source: :user
 
   validates :name, presence: true
