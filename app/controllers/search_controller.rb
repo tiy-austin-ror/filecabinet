28e6e63e6204030c51c575bba73a7c3da1b093c7
Matt Rice <mattrice12@outlook.com>
2016-08-10 18:44:37 -0500

    can't figure out cucumber testing

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,16 @@
 class SearchController < ApplicationController
   def search
-    render
+    if params[:search]
+      categories = Category.where("name ~* '.*#{params[:search]}.*'")
+      notes = Note.where("name ~* '.*#{params[:search]}.*'").order(:updated_at)
+      photos = Photo.where("name ~* '.*#{params[:search]}.*'").order(:updated_at)
+      users = User.where("name ~* '.*#{params[:search]}.*'").order("created_at DESC")
+      tags = Tag.where("name ~* '.*#{params[:search]}.*'")
+    else
+      categories = []
+      notes = Note.where(user: current_user)
+      photos = Photo.where(user: current_user)
+    end
+    render "shared/_nav.html.erb", locals: { categories: categories, notes: notes, photos: photos, users: users, tags: tags }
   end
 end
