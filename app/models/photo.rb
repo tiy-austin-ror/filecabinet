4d955ea66fa055481f4e36b6a61ddb9909432ca5
Robert Luckfield <rluck0419@gmail.com>
2016-08-10 14:00:53 -0500

    testing for s3

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -16,4 +16,12 @@ class Photo < ApplicationRecord
   validates :name, presence: true
   validates :desc, presence: true
   validates :upload, attachment_presence: true
+
+  has_attached_file :download,
+                  :storage => :s3,
+                  :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+
+  def s3_credentials
+    {:bucket => "filecabinet-tiy-ror", :aws_access_key_id => ENV["AWS_ACCESS_KEY_ID"], :aws_secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"]}
+  end
 end
---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,9 @@
+Rails.application.config.before_initialize do
+  Paperclip::Attachment.default_options[:url] = ':filecabinet-tiy-ror.s3.amazonaws.com'
+
+  Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+end
+
+Paperclip::Attachment.default_options[:s3_host_name] = 'us-standard.amazonaws.com'
+Paperclip::Attachment.default_options[:s3_region] = 'us-east-1'
+Paperclip::Attachment.default_options[:bucket] = 'filecabinet-tiy-ror'
