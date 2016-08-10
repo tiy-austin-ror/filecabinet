460e72382f5ae8e0580effebe82c2b8a6bf757a6
Kate Walters <kawalters01@gmail.com>
2016-08-10 11:53:16 -0500

    Migrated teams

--- db/migrate/20160810155113_create_teams.rb
+++ db/migrate/20160810155113_create_teams.rb
@@ -1,6 +1,7 @@
 class CreateTeams < ActiveRecord::Migration[5.0]
   def change
     create_table :teams do |t|
+      t.belongs_to :user, foreign_key: true
       t.string :name, null: false
 
       t.timestamps
